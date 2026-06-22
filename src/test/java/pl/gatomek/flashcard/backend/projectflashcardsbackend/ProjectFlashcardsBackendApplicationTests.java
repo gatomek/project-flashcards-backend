adde849c2a768319e27c0e4c73a01f04f2a43032
Gatomek <tomasz.galinski@gmail.com>
2026-06-22 21:40:56 +0200

    cr

--- src/test/java/pl/gatomek/flashcard/backend/projectflashcardsbackend/ProjectFlashcardsBackendApplicationTests.java
+++ src/test/java/pl/gatomek/flashcard/backend/projectflashcardsbackend/ProjectFlashcardsBackendApplicationTests.java
@@ -2,8 +2,10 @@ package pl.gatomek.flashcard.backend.projectflashcardsbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ghrepo")
 class ProjectFlashcardsBackendApplicationTests {
 
     @Test
